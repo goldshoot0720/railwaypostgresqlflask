75937bb0a6fe781dd28905128fde3e5165e2f5cf
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-26 18:45:04 +0800

    20250726 v1

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify
+import os
+from dotenv import load_dotenv
+import psycopg
+from psycopg import sql
+
+app = Flask(__name__)
+load_dotenv()
+
+def select_table(table_name):
+    # 使用 with 確保連線關閉
+    with psycopg.connect(os.getenv("DATABASE_URL")) as conn:
+        with conn.cursor() as cur:
+            query = sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name))
+            cur.execute(query)
+            rows = cur.fetchall()
+    return rows
+
+@app.route("/")
+def index():
+    return """
+<ul>
+<li><a href="/article">article</a></li>
+<li><a href="/bank">bank</a></li>
+<li><a href="/cloud">cloud</a></li>
+<li><a href="/experience">experience</a></li>
+<li><a href="/article">article</a></li>
+<li><a href="/food">food</a></li>
+<li><a href="/host">host</a></li>
+<li><a href="/inventory">inventory</a></li>
+<li><a href="/mail">mail</a></li>
+<li><a href="/member">member</a></li>
+<li><a href="/routine">routine</a></li>
+<li><a href="/subscription">subscription</a></li>
+<li><a href="/video">video</a></li>
+<ul>
+
+"""
+
+@app.route("/<table_name>")
+def get_table(table_name):
+    try:
+        rows = select_table(table_name)
+        return jsonify(rows)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ railwaypostgresqlflask-git.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\OneDrive\Documents\railwaypostgresqlflask"
+git init
+git add .
+git commit -m "20250726 v1"
+git remote add origin https://github.com/goldshoot0720/railwaypostgresqlflask.git
+git checkout -b main
+git push -u origin main --force
+pause
