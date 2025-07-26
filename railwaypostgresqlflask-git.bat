346f523ce95ce7d37b3fd1c779d4fea49a93460d
goldshoot0720 <goldshoot0720@gmail.com>
2025-07-26 18:59:10 +0800

    20250726 v3

---
+++ railwaypostgresqlflask-git.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\OneDrive\Documents\railwaypostgresqlflask"
+git init
+git add .
+git commit -m "20250726 v3"
+git remote add origin https://github.com/goldshoot0720/railwaypostgresqlflask.git
+git checkout -b master
+git push -u origin  master --force
+pause
